c80e4fed037e941de606d36154723f0587c21e2c
Abiola Martins <hsemartins@gmail.com>
2023-03-08 08:32:52 +0300

    Modify index.html, style.css and script/menu.js for Client Validation.

--- script/menu.js
+++ script/menu.js
@@ -18,3 +18,28 @@ closeButton.addEventListener('click', () => {
   menuSection.classList.remove('visible');
   menuBtn.style.display = 'block';
 });
+
+// *******************************Cient Validation Start here.*******************************
+const contact = document.getElementById('form');
+
+const isUpperCaseEmail = (email) => {
+  if (/[A-Z]/.test(email)) {
+    return true;
+  }
+  return false;
+};
+
+contact.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const email = event.target.email.value;
+  const errorMsg = document.querySelector('.errorsDisplay');
+  const validEmail = !isUpperCaseEmail(email);
+
+  if (validEmail) {
+    errorMsg.style.display = 'none';
+    contact.submit();
+  } else {
+    errorMsg.style.display = 'block';
+    errorMsg.innerHTML = 'Please write email in small letters';
+  }
+});
